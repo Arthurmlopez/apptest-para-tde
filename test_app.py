a1aed0f1a2d16b5176e4b0dd7b93518e6533cec7
Arthurmlopez <89947351+Arthurmlopez@users.noreply.github.com>
2021-09-02 16:41:23 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 from myapp import soma
 
-@pytest.mark.parametrize('n1,n2,expected', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), (dois,0,2), (2,,2)])
+@pytest.mark.parametrize('n1,n2,expected', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ("dois",0,2), (2,,2)])
 def test_soma(n1,n2, expected):
     assert soma(n1,n2) == expected
