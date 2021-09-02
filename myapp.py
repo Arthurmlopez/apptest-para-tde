93ba6390f1d86d0290491743bdda3938689e67dd
Arthurmlopez <89947351+Arthurmlopez@users.noreply.github.com>
2021-09-02 16:46:02 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,12 @@
-def soma(n1,n2):
-  return n1 + n2
-
- if n1 == None:
-       n1 = n2
-    
+def soma(n1,n2):  
  if is_number(n1) and is_number(n2):
    return float(n1) + float(n2)
  else:
    return None
+  
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
